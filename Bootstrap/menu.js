936401c0a75f27f3ed4f966e7df2d67784f185b3
Kaleakash <akash300383@gmail.com>
2021-03-28 23:22:58 +0530

    Done

--- Bootstrap/menu.js
+++ Bootstrap/menu.js
@@ -1,7 +1,8 @@
 function add() {
     var title= document.getElementById("title").value;
+    
     console.log(title);
-
+    for(var i=0;i<10;i++){
     var pNode = document.createElement("p");//create p tag 
     pNode.setAttribute("class","abc");  // add attribute for p tag 
     var pText = document.createTextNode(title);   //create text 
@@ -9,6 +10,6 @@ function add() {
 
 
     document.getElementById("main").appendChild(pNode); // add p to main tag 
-
+    }
 
 }
